48fa8c3e1d88858e4fb529ca83a65cdbfead61b9
Akiram <akiram8112@gmail.com>
2025-04-30 02:29:51 -0700

    another optimization

--- src/pages/api/albums/index.ts
+++ src/pages/api/albums/index.ts
@@ -23,6 +23,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json(albums);
   } catch (error) {
-    // res.status(500).json({ error: 'Failed to fetch albums' });
+    res.status(500).json({ error: 'Failed to fetch albums' });
   }
 }
